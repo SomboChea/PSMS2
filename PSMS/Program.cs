183ddb6a25b509866fc85cae90c8872aab2cd276
Sambo Chea <sombochea100@gmail.com>
2017-09-13 17:03:49 +0700

    add startup

--- PSMS/Program.cs
+++ PSMS/Program.cs
@@ -17,7 +17,7 @@ namespace PSMS
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             
-            Application.Run(new frmLogin());
+            Application.Run(new frmSlashScreen());
 
         }
     }
